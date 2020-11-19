116f7fa7c7078a2fccdeb0440d68bf7706132358
Daniil Motsnyi <daniilmotsnyi@gmail.com>
2020-11-19 22:45:30 +0200

    first test

---
+++ final_task/tests/test_app.py
@@ -0,0 +1,27 @@
+from app import app
+import unittest
+
+
+class TestApp(unittest.TestCase):
+    def setUp(self):
+        # creates a test client
+        self.app = app.test_client()
+        # propagate the exceptions to the test client
+        self.app.testing = True
+
+    def test_home_status_code(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+        # assert the status code of the response
+        self.assertEqual(result.status_code, 200)
+
+    def test_home_data(self):
+        # sends HTTP GET request to the application
+        # on the specified path
+        result = self.app.get('/')
+
+
+if __name__ == "__main__":
+    unittest.main()
